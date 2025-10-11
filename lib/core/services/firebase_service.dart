1607e97552ed07dcd2fa0305478a9e194f886013
Leonard <daviddranoel@gmail.com>
2025-10-11 16:02:23 +0200

    added firebase services - a firebase helper that centralizes firebase instances for cleaner code across the app

---
+++ lib/core/services/firebase_service.dart
@@ -0,0 +1,15 @@
+// lib/core/services/firebase_service.dart
+//
+// --------------------------------------------------------
+// Centralized Firebase instance access
+// --------------------------------------------------------
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseService {
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static FirebaseStorage storage = FirebaseStorage.instance;
+}
